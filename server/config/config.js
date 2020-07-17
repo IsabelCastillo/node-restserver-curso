6403356b178a4b75a9af1da34d3197c4d508affc
IsabelCastillo <isabelcastillo0214@gmail.com>
2020-07-17 14:56:43 -0500

    Variable de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -12,7 +12,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://isabel_c:COp3V4TQI2pDEOGy@cluster0.lb1lo.mongodb.net/cafe';
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
